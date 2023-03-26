2eeecfd0150b126b2e29cbb736d3ab99eaf9608e
cristi <scarlat.cristian.gabriel@gmail.com>
2023-03-26 22:49:21 +0300

    initial commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import ReactDom from "react-dom";
+import React from "react";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+
+const root = document.querySelector("#app");
+
+ReactDom.render(<p>This is my boiler plate</p>, root);
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        loader: "babel-loader",
+        // looking for any file that ends in .js
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+      {
+        // looking for any file that ends in .css
+        test: /\.s?css$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  devtool: "cheap-module-eval-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+  },
+};
