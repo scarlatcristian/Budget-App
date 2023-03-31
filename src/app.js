c6cdb55e5ba32c864b139c48308cc0cc38fbf434
cristi <scarlat.cristian.gabriel@gmail.com>
2023-03-31 22:59:58 +0300

    added rendering for expenses items

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
+import { Provider } from "react-redux";
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
 import { addExpense } from "./actions/expenses";
@@ -8,15 +9,24 @@ import getVisibleExpenses from "./selectors/expenses";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-const root = document.querySelector("#app");
 const store = configureStore();
 
-store.dispatch(addExpense({ description: "Water bill" }));
-store.dispatch(addExpense({ description: "Gas bill" }));
+store.dispatch(addExpense({ description: "Water bill", amount: 4500 }));
+store.dispatch(addExpense({ description: "Gas bill", amount: 2222 }));
 store.dispatch(setTextFilter("gas"));
+
+setTimeout(() => {
+  store.dispatch(setTextFilter(""));
+}, 3000);
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 
-ReactDom.render(<AppRouter />, root);
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+ReactDom.render(jsx, document.querySelector("#app"));
---
+++ src/components/ExpenseListItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { connect } from "react-redux";
+import { removeExpense } from "../actions/expenses";
+
+const ExpenseListItem = ({ dispatch, id, description, amount, createAt }) => (
+  <div>
+    <h3>{description}</h3>
+    <p>
+      {amount} - {createAt}
+    </p>
+    <button
+      onClick={() => {
+        dispatch(removeExpense({ id }));
+      }}
+    >
+      Remove
+    </button>
+  </div>
+);
+
+export default connect()(ExpenseListItem);
