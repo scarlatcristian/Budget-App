1033d66850919eb346733a709e5b50a4372a9e2f
cristi <scarlat.cristian.gabriel@gmail.com>
2023-03-31 19:44:13 +0300

    added Redux store, actions and reducers

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 import React from "react";
 import ReactDom from "react-dom";
 import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
+import { addExpense } from "./actions/expenses";
+import { setTextFilter } from "./actions/filters";
+import getVisibleExpenses from "./selectors/expenses";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
 const root = document.querySelector("#app");
+const store = configureStore();
+
+store.dispatch(addExpense({ description: "Water bill" }));
+store.dispatch(addExpense({ description: "Gas bill" }));
+store.dispatch(setTextFilter("gas"));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
 
 ReactDom.render(<AppRouter />, root);
