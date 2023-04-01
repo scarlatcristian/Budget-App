9fd8a073daada23b2904d6ea7d9c870ba8413089
cristi <scarlat.cristian.gabriel@gmail.com>
2023-04-01 23:16:27 +0300

    added datePicker with moment.js and react-dates

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,10 +1,18 @@
 import React from "react";
+import moment from "moment";
+import { SingleDatePicker } from "react-dates";
+import "react-dates/lib/css/_datepicker.css";
+
+const now = moment();
+console.log(now.format("Do MMM YYYY"));
 
 export default class ExpenseForm extends React.Component {
   state = {
     description: "",
     note: "",
     amount: "",
+    createdAt: moment(),
+    calendarFocus: false,
   };
   onDescriptionChange = (e) => {
     const description = e.target.value;
@@ -18,6 +26,12 @@ export default class ExpenseForm extends React.Component {
     const amount = e.target.value;
     if (amount.match(/^\d*(\.\d{0,2})?$/)) this.setState(() => ({ amount }));
   };
+  onDateChange = (createdAt) => {
+    this.setState(() => ({ createdAt }));
+  };
+  onFocusChange = ({ focused }) => {
+    this.setState(() => ({ calendarFocus: focused }));
+  };
   render() {
     return (
       <div>
@@ -29,17 +43,29 @@ export default class ExpenseForm extends React.Component {
             value={this.state.description}
             onChange={this.onDescriptionChange}
           />
+
           <input
             type="text"
             placeholder="Amount"
             value={this.state.amount}
             onChange={this.onAmountChange}
           />
+
+          <SingleDatePicker
+            date={this.state.createdAt}
+            onDateChange={this.onDateChange}
+            focused={this.state.calendarFocus}
+            onFocusChange={this.onFocusChange}
+            numberOfMonths={1}
+            isOutsideRange={() => false}
+          />
+
           <textarea
             placeholder="Add a note for your expense (optional)"
             value={this.state.note}
             onChange={this.onNoteChange}
           ></textarea>
+
           <button>Add Expense</button>
         </form>
       </div>
