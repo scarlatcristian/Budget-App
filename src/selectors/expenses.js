e389829a6c0f76af77f21446a9661bf111f4880b
cristi <scarlat.cristian.gabriel@gmail.com>
2023-04-02 22:33:05 +0300

    added filter by dates calendar

--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -1,11 +1,16 @@
+import moment from "moment";
+
 // Get visible expenses
 const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
   return expenses
     .filter((expense) => {
-      const startDateMatch =
-        typeof startDate !== "number" || expense.createdAt >= startDate;
-      const endDateMatch =
-        typeof endDate !== "number" || expense.createdAt <= endDate;
+      const createAtMoment = moment(expense.createdAt);
+      const startDateMatch = startDate
+        ? startDate.isSameOrBefore(createAtMoment, "day")
+        : true;
+      const endDateMatch = endDate
+        ? endDate.isSameOrAfter(createAtMoment, "day")
+        : true;
       const textMatch = expense.description
         .toLowerCase()
         .includes(text.toLowerCase());
